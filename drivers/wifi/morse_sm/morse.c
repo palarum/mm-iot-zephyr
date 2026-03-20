2f04586f6afba5876feba1a19a98a5d0e2c870ab
palarum <palarum@Ubuntu24-Base.localdomain>
2026-03-20 20:01:22 +0000

    morse_sm: restore SPI-DT_SPEC_INT_GET delay arg for Zephyr 4.2

--- drivers/wifi/morse_sm/morse.c
+++ drivers/wifi/morse_sm/morse.c
@@ -551,7 +551,8 @@ static const struct net_wifi_mgmt_offload morse_api = {
 const struct morse_config conf = {
 	.spi = SPI_DT_SPEC_INST_GET(0,
 				    (SPI_LOCK_ON | SPI_OP_MODE_MASTER | SPI_TRANSFER_MSB |
-				     SPI_WORD_SET(SPI_FRAME_BITS))),
+				     SPI_WORD_SET(SPI_FRAME_BITS)),
+				    0),
 	.resetn = GPIO_DT_SPEC_INST_GET(0, resetn_gpios),
 	.wakeup = GPIO_DT_SPEC_INST_GET(0, wakeup_gpios),
 	.busy = GPIO_DT_SPEC_INST_GET(0, busy_gpios),
